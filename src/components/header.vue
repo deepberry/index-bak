72ff1c5aa897375f8d059ea9e0db2b927c69b550
iRuxu <rx6@qq.com>
2022-08-05 05:06:23 +0800

    feat:storybook

---
+++ src/components/header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="c-header">
+        <img class="m-logo" :src="logo" alt="深莓智能" />
+        <h1 class="m-title">DeepBerry Dashboard</h1>
+    </div>
+</template>
+
+<script>
+import { getCdnLink } from "@deepberry/common/js/utils";
+export default {
+    name: "CommonHeader",
+    props: [],
+    components: {},
+    data: function () {
+        return {};
+    },
+    computed: {
+        logo: function () {
+            return getCdnLink("img/common/logo/blue.svg");
+        },
+    },
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {},
+};
+</script>
---
+++ src/views/Storybook.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="p-storybook">
+        <Header />
+
+        <div class="m-box wp">
+            <ul class="m-storybook-list u-list">
+                <li class="u-item">
+                    <b>vue3-element-extend</b>
+                    <em>公共扩展组件库</em>
+                    <a href="https://github.com/deepberry/vue3-element-extend" target="_blank">README</a>
+                    <a href="https://admin.deepberry.cn/vue3-element-extend" target="_blank">API REFERENCE</a>
+                </li>
+                <li class="u-item">
+                    <b>titan-web-components</b>
+                    <em>泰坦业务组件库</em>
+                    <a href="https://github.com/deepberry/titan-web-components" target="_blank">README</a>
+                    <a href="https://admin.deepberry.cn/titan-web-components" target="_blank">API REFERENCE</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/header.vue";
+export default {
+    name: "StorybookPage",
+    props: [],
+    components: { Header },
+    data: function () {
+        return {};
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    created: function () {},
+    mounted: function () {},
+};
+</script>
+
+<style lang="less">
+@import "@/assets/css/storybook.less";
+</style>
