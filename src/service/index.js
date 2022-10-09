ce4367dc4891be02936676349abf6ad891046dc1
litthy <litthy@qq.com>
2022-10-09 17:40:05 +0800

    fix: service

--- src/service/index.js
+++ src/service/index.js
@@ -1,12 +1,7 @@
 import { $cms } from "@deepberry/common/js/https";
 
 function getDashboardList() {
-    let isDev = process.env.NODE_ENV === "development";
-    return $cms().get("/api/cms/misc/dashboard", {
-        params: {
-            __no_cache: isDev ? 1 : 0,
-        },
-    });
+    return $cms().get("/api/cms/misc/dashboard");
 }
 
 export { getDashboardList };
